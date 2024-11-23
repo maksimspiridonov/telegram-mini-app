0c2521e858052fe4590e5309d907f5925dc55a94
maksimspiridonov <maksim.spiridon0v@yandex.ru>
2024-11-23 21:55:28 +0500

    Add basic structure and code

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Инициализация Telegram Web App
-const tg = window.Telegram.WebApp;
-
-// Убедимся, что приложение готово
-tg.ready();
-
 // Промокод, который будет скопирован
 const promoCode = "6fgw";
 
 // URL партнера
 const partnerUrl = "https://1warlo.top/casino/list/4?p=" + promoCode;
 
-// Функция для копирования промокода
+// Функция для копирования промокода и перенаправления на сайт партнера
 function copyPromoCode() {
     // Копируем промокод в буфер обмена
     navigator.clipboard.writeText(promoCode).then(() => {
         // Информируем пользователя о том, что промокод скопирован
         alert("Promo code copied: " + promoCode);
 
-        // Открываем ссылку партнера внутри Telegram Web App
-        tg.openLink(partnerUrl);
+        // Перенаправляем на сайт партнера
+        window.location.href = partnerUrl;
     }).catch(err => {
         console.error("Failed to copy promo code: ", err);
     });
